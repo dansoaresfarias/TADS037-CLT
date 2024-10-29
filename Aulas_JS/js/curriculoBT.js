ace7ebb4e012ff2466138c6a91f0449299f0f9c5
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-10-29 23:10:41 +0000

    Aula de JS

---
+++ Aulas_JS/js/curriculoBT.js
@@ -0,0 +1,9 @@
+function hackear(params) {
+    var elementoNome = document.getElementById("nome");
+    elementoNome.innerHTML = "Matheus Monteiro Lima";
+
+    var elementoFoto = document.getElementById("foto");
+    elementoFoto.src = "imgs/matheusMonteiro.jpg"
+
+    document.getElementById("apresentacao").style.background = "blue";
+}
